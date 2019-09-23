a9df668eea3f18fe7774af9cc89018c3fe181c8a
colefin8 <cole.fin8@gmail.com>
2019-09-23 16:23:33 -0600

    I added perfect square box

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -4,6 +4,7 @@ import FilterObject from '../Topics/FilterObject'
 import FilterString from '../Topics/FilterString'
 import Palindrome from '../Topics/Palindrome'
 import Sum from '../Topics/Sum'
+import IsSquare from '../Topics/IsSquare'
 
 class TopicBrowser extends Component{
 render(){
@@ -14,6 +15,7 @@ render(){
             <FilterString/>
             <Palindrome/>
             <Sum/>
+            <IsSquare/>
         </div>
         )
     }
---
+++ src/components/Topics/IsSquare.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+
+class IsSquare extends Component {
+    constructor(){
+        super();
+        this.state = {
+            userInput: "",
+            isSquare: ""
+        }
+    }
+
+    changeInput = (value) => {
+        this.setState({userInput: value})
+    }
+
+    solve = (num) =>{
+        if (Math.sqrt(parseInt(num)) % 1 === 0){
+            this.setState({isSquare: "It's a perfect square!"})
+        } else {
+            this.setState({isSquare: "It's not a perfect square..."})
+        }
+
+    }
+
+    render(){
+        return(
+            <div className="puzzleBox squarePB">
+            <h4>Square</h4>
+            <input className="inputLine" onChange={(event) => this.changeInput(event.target.value)}/>
+            <button className="confirmationButton" onClick={() => this.solve(this.state.userInput)}>Is it a perfect square?</button>
+            <span className="resultsBox">{this.state.isSquare}</span>
+        </div>
+        )
+    }
+}
+
+export default IsSquare
